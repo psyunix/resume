db971434c42b5fceff2cf52c09ab9c224c52a669
psyunix <psyunix@gmail.com>
2025-04-30 18:21:18 +0200

    certs dir and az 104

--- data/profile.js
+++ data/profile.js
@@ -214,13 +214,13 @@ Led the architecture, deployment, and maintenance of SAS Viya environments acros
   ],
   certifications: [
     {
-      desc: `<strong>AZ-104 Microsoft Azure Administrator</strong> by <strong>Udemy</strong> (<a target='_blank' rel='noreferrer' href='https://www.udemy.com/certificate/UC-d6b1b188-ea79-4eb8-bc42-9e3517196abc'>https://goo.gl/X3HEdR</a>)`,
+      desc: `<strong>AZ-104 Microsoft Azure Administrator</strong> by <strong>Udemy</strong> (<a target='_blank' rel='noreferrer' href='https://www.udemy.com/certificate/UC-d6b1b188-ea79-4eb8-bc42-9e3517196abc'>AZ 104</a>)`,
       date: "2022",
     },
 
     {
       desc: `<strong>VMware VCTA DataCenter</strong>, a  by <em>VMware</em>.
-       (<a target='_blank' rel='noreferrer' href='https://github.com/psyunix/resume/blob/main/data/AZ-104-milan_cvetic_udemy.pdf'>VMware VCTA - DCV</a>)`,
+       (<a target='_blank' rel='noreferrer' href='https://github.com/psyunix/resume/blob/main/data/certs/AZ-104-milan_cvetic_udemy.pdf'>VMware VCTA - DCV</a>)`,
       date: "2021",
     },
   ],
