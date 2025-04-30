08d5c1f63f8f87e95ddf39833213859fca85ea52
psyunix <psyunix@gmail.com>
2025-04-30 20:57:52 +0200

    others

--- data/profile.js
+++ data/profile.js
@@ -56,7 +56,7 @@ const profileData = {
     },
     {
       title: "Others",
-      value: "macOS, Unix/Linux, Microsoft (10+ years), Backup and Data management (10+ years), VMware and Cloud technologies (10+ years), Security (10+ years), Monitoring systems (10+ years), Network (10+ years), Python/Perl/Bash/PowerShell scripting (10+ years), DevOps (5+ years)",
+      value: "Terraform, Ansible, Grafana and Prometheus, Backup and Data management (10+ years), VMware and Cloud technologies (10+ years), Security (10+ years), Monitoring systems (10+ years), Network (10+ years), Python/Perl/Bash/PowerShell scripting (10+ years), DevOps (5+ years)",
     },
   ],
   experiences: [
